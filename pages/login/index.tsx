57d54d5170f4f4fe0779a18c7b206fefad15505b
elle510 <elle0510@gmail.com>
2021-12-17 12:13:22 +0900

    로그인/로그아웃 누락파일

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 // import Link from 'next/link';
 
-import LoginForm from '../components/LoginForm';
+import LoginForm from '../../components/LoginForm';
 
 const Login = () => (
   <>
